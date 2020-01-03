335012630631c72febfe4bc85d9516d291c8b204
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-03 19:23:30 +0200

    feat(app): create AppRoutingModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/auth/sign-in',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
