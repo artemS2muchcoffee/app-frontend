ec46d982e41f42587ebb0e1db6a188e08775c048
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-05 15:04:15 +0200

    feat(core): add NgxsStoreModule to CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgxsStoreModule } from '../ngxs/ngxs.module';
+
 @NgModule({
   imports: [
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    NgxsStoreModule,
   ],
 })
 export class CoreModule {
