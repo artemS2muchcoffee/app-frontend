257bb53cc26ed89dd6cc19dfd73a6691fb63a38a
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-05 14:02:12 +0200

    feat(shared): add ReactiveFormsModule and FormValidationErrorModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FormValidationErrorModule } from './modules/form-validation-error/form-validation-error.module';
 
 @NgModule({
   exports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormValidationErrorModule,
   ],
 })
 export class SharedModule {
