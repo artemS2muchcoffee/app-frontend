82121bd9c4349ee6b093078eb821b3d8f4961bd2
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-05 15:07:10 +0200

    feat(auth:sign-in): create functionality for login

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,9 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from '../../core/services/auth.service';
+import { FormError } from '../../shared/models/form-error.model';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+
+  signInForm: FormGroup;
+  showErrorsIfSubmitted: boolean;
+  public errors: FormError;
+
+  get usernameControl(): AbstractControl {
+    return this.signInForm.get('username');
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.signInForm.get('password');
+  }
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.setErrors();
+    this.createSignInForm();
+    this.showErrorsIfSubmitted = false;
+  }
+
+  createSignInForm() {
+    const validationFormValue = {
+      username: {
+        minlength: 4,
+      },
+      password: {
+        minlength: 6,
+      },
+    };
+
+    this.signInForm = new FormGroup({
+      username: new FormControl( '', [
+          Validators.required,
+          Validators.minLength(validationFormValue.username.minlength),
+        ],
+      ),
+      password: new FormControl( '', [
+          Validators.required,
+          Validators.minLength(validationFormValue.password.minlength),
+        ],
+      ),
+    });
+  }
+
+  onSubmit() {
+    this.showErrorsIfSubmitted = true;
+    if (this.signInForm.valid) {
+      this.authService.signIn(this.signInForm.value);
+    }
+  }
+
+  setErrors() {
+    this.errors = {
+      username: [
+        {name: 'required', text: 'Username is required'},
+        {name: 'minlength', text: `Min length is 4 symbols`},
+      ],
+      password: [
+        {name: 'required', text: 'Password is required'},
+        {name: 'minlength', text: `Min length is 6 symbols`},
+      ],
+    };
+  }
 }
--- src/app/auth/sign-in/sign-in.module.ts
+++ src/app/auth/sign-in/sign-in.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+} from '@angular/material';
+
 import { SignInComponent } from './sign-in.component';
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SharedModule } from '../../shared/shared.module';
@@ -11,6 +17,9 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     SignInRoutingModule,
     SharedModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
   ],
 })
 export class SignInModule {
