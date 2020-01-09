950032e669c004f94c019481e437332a29d830d9
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-09 19:51:43 +0200

    feat(ngxs): add states to NgxsStoreModule

--- src/app/ngxs/ngxs.module.ts
+++ src/app/ngxs/ngxs.module.ts
@@ -8,15 +8,27 @@ import { environment } from '../../environments/environment';
 import { AuthState } from './auth/auth.state';
 import { SignInRequestState } from './requests/auth/sign-in/sign-in.request.state';
 import { SignUpRequestState } from './requests/auth/sign-up/sign-up.request.state';
+import { ApplicationState } from './application/application.state';
+import { ProductState } from './product/product.state';
+import { ReviewState } from './review/review.state';
+import { GetProductsRequestState } from './requests/product/get-products/get-products.request.state';
+import { GetProductReviewsRequestState } from './requests/review/get-product-reviews/get-product-reviews.request.state';
+import { PostProductReviewRequestState } from './requests/review/post-product-review/post-product-review.request.state';
 
 @NgModule({
   imports: [
     NgxsModule.forRoot(
     [
       AuthState,
+      ApplicationState,
+      ProductState,
+      ReviewState,
       // requests
       SignInRequestState,
       SignUpRequestState,
+      GetProductsRequestState,
+      GetProductReviewsRequestState,
+      PostProductReviewRequestState,
     ],
       { developmentMode: !environment.production },
     ),
