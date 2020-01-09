f8919c3127ea1ac756ca72bf4b62d595c95b36ac
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-09 20:14:38 +0200

    feat(app): add ProductModule to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IsNotAuthenticatedGuard } from './core/guards/is-not-authenticated.guard';
+
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule),
+    canLoad: [IsNotAuthenticatedGuard],
+    canActivate: [IsNotAuthenticatedGuard],
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then(mod => mod.ProductModule),
   },
   {
     path: '**',
-    redirectTo: '/auth/sign-in',
+    redirectTo: '/product/product-list',
   }
 ];
 
