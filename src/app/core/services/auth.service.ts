fb74f29ab8aecc29c1979b575240c1f680869bbb
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-09 19:53:14 +0200

    feat(core:services): create selects and logOut method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,8 +3,11 @@ import { Injectable } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 
-import { SignIn, SignUp } from '../../ngxs/auth/auth.actions';
+import { ClearToken, SignIn, SignUp } from '../../ngxs/auth/auth.actions';
 import { AuthGetterState } from '../../ngxs/auth/auth-getter.state';
+import { SignInRequestGetterState } from '../../ngxs/requests/auth/sign-in/sign-in-request-getter.state';
+import { IRequestsNestedState } from '../../ngxs/requests/requests.interface';
+import { SignUpRequestGetterState } from '../../ngxs/requests/auth/sign-up/sign-up-request-getter.state';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +16,12 @@ export class AuthService {
 
   @Select(AuthGetterState.isGuest)
   isGuest$: Observable<boolean>;
+
+  @Select(SignInRequestGetterState.getSignInRequestState)
+  signInRequestState$: Observable<IRequestsNestedState>;
+
+  @Select(SignUpRequestGetterState.getSignUpRequestState)
+  signUpRequestState$: Observable<IRequestsNestedState>;
 
   constructor(
     private store: Store,
@@ -27,4 +36,7 @@ export class AuthService {
     this.store.dispatch(new SignIn(data));
   }
 
+  logOut() {
+    this.store.dispatch(new ClearToken());
+  }
 }
