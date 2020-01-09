ef460ef8d833d0f800d4ee4774405708ef10de30
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-09 19:45:14 +0200

    feat(ngxs:request): add request states

--- src/app/ngxs/requests/requests.state.ts
+++ src/app/ngxs/requests/requests.state.ts
@@ -2,6 +2,9 @@ import { State } from '@ngxs/store';
 
 import { SignInRequestState } from './auth/sign-in/sign-in.request.state';
 import { SignUpRequestState } from './auth/sign-up/sign-up.request.state';
+import { GetProductsRequestState } from './product/get-products/get-products.request.state';
+import { GetProductReviewsRequestState } from './review/get-product-reviews/get-product-reviews.request.state';
+import { PostProductReviewRequestState } from './review/post-product-review/post-product-review.request.state';
 
 @State<{}>({
   name: 'requests',
@@ -9,6 +12,9 @@ import { SignUpRequestState } from './auth/sign-up/sign-up.request.state';
   children: [
     SignInRequestState,
     SignUpRequestState,
+    GetProductsRequestState,
+    GetProductReviewsRequestState,
+    PostProductReviewRequestState,
   ],
 })
 export class RequestsState {
