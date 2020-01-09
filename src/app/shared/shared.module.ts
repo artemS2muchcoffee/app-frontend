856c3a8510734a42944878bc6b7deed33896632e
Artem Serdiuk <artem.s@2muchcoffee.com>
2020-01-09 20:02:29 +0200

    feat(shared): add AuthFormModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormValidationErrorModule } from './modules/form-validation-error/form-validation-error.module';
+import { AuthFormModule } from './modules/auth-form/auth-form.module';
 
 @NgModule({
   exports: [
     CommonModule,
     ReactiveFormsModule,
     FormValidationErrorModule,
+    AuthFormModule,
   ],
 })
 export class SharedModule {
